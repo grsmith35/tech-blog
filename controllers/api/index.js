0fa6dde6542b6936fa1e5d6c45c333bf3c92e7de
Riley Smith <gordonrileysmith@gmail.com>
2021-10-31 23:04:24 -0600

    added some handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,4 +4,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
 
+router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+
 module.exports = router;
